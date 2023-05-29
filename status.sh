553b4ff7373b4c157a38afac9ee4e2718aa266d0
Luke A.C.A. Rieff <webmaster@fannst.nl>
2023-05-29 13:56:42 +0200

    Updated config

--- status.sh
+++
@@ -1,6 +0,0 @@
-uptime=$(uptime | cut -d ',' -f1 | cut -d ' ' -f4,5)
-date=$(date +"%a %F %H: %M")
-linux_version=$(uname -r | cut -d '-' -f1)
-battery_status=$(cat /sys/class/power_supply/BAT0/status)
-
-echo $uptime - $linux_version - $battery_status - $date
